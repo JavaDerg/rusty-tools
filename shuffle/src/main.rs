1b8906473bdbc8dd08f74647b1bfcfa92adc4499
PostRex <post-rex@pm.me>
2020-01-16 00:08:29 +0100

    Actual Initial commit

---
+++ shuffle/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let matches = clap::App::new("")
+        .name("shuffle")
+        .about("Shuffle line of files or steam input")
+        .get_matches();
+    /** TODO:
+        - File input
+        - Steam input
+        - CLI input
+        - Output file
+        - Output steam
+        - Custom line termination
+        - Line can occur multiple times
+        - Custom random source
+        - Input range
+        - Amount of lines to output
+    */
+}
